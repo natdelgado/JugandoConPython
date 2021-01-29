5e86678df23d0ee6e1b70fbda542b740b9a3e5d5
natdelgado <carmendelgado1601@gmail.com>
2021-01-29 14:54:16 -0300

    14:54 29/1/2021

--- Calculadora.py
+++ Calculadora.py
@@ -1,5 +1,25 @@
-"""Calculadora sencilla que al ingresar dos números y una operación muestra el resultado"""
-numero_1 = int(input("Ingrese el primer número: "))
-operacion = input()
-numero_2 = int(input("Ingrese el segundo número: "))
+#Calculadora
 
+"""Calculadora sencilla que al ingresar dos numeros 
+y una operacion muestra el resultado"""
+
+x = int(input("Ingrese el primer numero: "))
+y = int(input("Ingrese el segundo numero: "))
+operacion = int(input("Eliga la operacion: 1(*) 2(/) 3(+) 4(-) "))
+
+def Operar():
+   if operacion == 1 :
+      resultado = x * y
+      
+   elif operacion == 2 :
+      resultado = x / y
+      
+   elif operacion == 3 :
+      resultado = x + y
+      
+   elif operacion == 4 :
+      resultado = x - y
+   print("Resultado " + str(resultado))
+   """else :
+      print("Escribe el numero de una operacion existente")"""
+Operar()        
